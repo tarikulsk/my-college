b8ef59421f448cfb9c49f589b08226f3d9562261
tarikulsk <tarikulsk4@gmai.com>
2023-07-23 02:38:10 +0530

    something change

--- src/component/Pages/Home/Research/Research.jsx
+++ src/component/Pages/Home/Research/Research.jsx
@@ -86,7 +86,7 @@ const Research = () => {
                         <p>If a dog chews shoes whose shoes does he choose?</p>
                         <div className="card-actions justify-end">
                             <div className="badge badge-outline">Fashion</div>
-                            {/* <div className="badge badge-outline">Products</div> */}
+                            <div className="badge badge-outline">Products</div>
                         </div>
                     </div>
                 </div>
